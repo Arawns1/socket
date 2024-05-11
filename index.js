edf353880e8b34e11bb110de8a8a6ea85c81a3d9
Gabriel Damico <gabrieldamico22@gmail.com>
2024-05-11 17:29:44 -0300

    chore: Netlify config

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require('express')
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 const { Server } = require('socket.io');
+const serverless = require('serverless-http');
 
+const router = express.Router()
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
 res.sendFile(join(__dirname, 'index.html'));
 });
 
@@ -23,6 +25,11 @@ io.on('connection', (socket) => {
       });
   });
 
+
+app.use("/.netlify/functions/index", router);
+
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+  console.log('server running at port 3000');
 });
+
+module.exports.handler = serverless(app);
